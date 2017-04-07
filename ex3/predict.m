d7033ef85c16f31e3ea657ebeb69264bd0dd3861
Prity Roy <pritywiz@users.noreply.github.com>
2017-04-07 21:13:27 +0530

    submitted the assignment with 100% accuracy

--- ex3/predict.m
+++ ex3/predict.m
@@ -20,7 +20,12 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
+X = [ones(m, 1) X];
 
+a2 = sigmoid(X*Theta1');
+a2 = [ones(size(a2,1), 1) a2];
+a3 = sigmoid(a2*Theta2');
+[a,p] =max(a3, [], 2);
 
 
 
--- ex3/predictOneVsAll.m
+++ ex3/predictOneVsAll.m
@@ -29,7 +29,8 @@ X = [ones(m, 1) X];
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
 %       
-
+Sig=sigmoid(X*all_theta');
+[a,p] =max(Sig, [], 2);
 
 
 
